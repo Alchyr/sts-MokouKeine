d2bbcbbda23b4d29755b12552c894eedc8849c42
Alchyr <IsithAlchyr@gmail.com>
2019-05-16 08:39:45 -0700

    adjust save prevention

--- src/main/java/もこけね/patch/helpers/NoSave.java
+++ src/main/java/もこけね/patch/helpers/NoSave.java
@@ -3,9 +3,11 @@ package もこけね.patch.helpers;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.SaveHelper;
 import もこけね.character.MokouKeine;
+import もこけね.patch.enums.CharacterEnums;
 
 @SpirePatch(
         clz = SaveHelper.class,
@@ -15,7 +17,7 @@ public class NoSave {
     @SpirePrefixPatch
     public static SpireReturn<Boolean> noYouShouldNotSave()
     {
-        if (AbstractDungeon.player instanceof MokouKeine)
+        if (CardCrawlGame.chosenCharacter == CharacterEnums.MOKOUKEINE)
         {
             return SpireReturn.Return(false);
         }
